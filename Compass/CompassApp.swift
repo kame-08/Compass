ea0a374836ccbf143f6ef57bc07d93d95c92b72c
Kame15-2021 <21cm0108@jec.ac.jp>
2022-07-24 12:57:27 +0900

    Initial Commit

---
+++ Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by Ryo on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
